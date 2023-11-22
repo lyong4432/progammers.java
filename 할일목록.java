cfb13f46b10ccd504cfa98f5aac77ad78d3e8da5
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-22 11:43:39 +0900

    Create 할일목록.java

---
+++ 할일목록.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList();
+        for (int i = 0 ; i < finished.length; i++){
+            if (finished[i] == false) answer.add(todo_list[i]);
+        }
+        return answer;
+    }
+}
