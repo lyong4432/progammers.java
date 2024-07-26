87f7e6bbabd5ff6069fbdea64932e271c9faa88a
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2024-07-26 16:08:55 +0900

    Create 자릿수_더하기.java

---
+++ 자릿수_더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String temp = Integer.toString(n);
+        for (int i = 0; i < temp.length(); i++){
+            answer += Integer.parseInt(String.valueOf(temp.charAt(i)));
+        }
+
+        return answer;
+    }
+}
